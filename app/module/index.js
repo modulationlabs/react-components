77273c34a9bdda4c9ee42e2a2d8d63294e0e6add
Christopher Hill <accounts@christopherhill.com>
2017-08-09 10:58:11 -0700

    More additions.

--- app/module/index.js
+++ app/module/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import './module.scss';
+import svg from './../../assets/sample.svg';
+
+console.log('svg: ', svg);
+
 class Module extends React.Component {
 
   render () {
     return (
-      <div>
+      <div className="module">
         <h1>Module</h1>
+        <img src={svg} alt="image" />
       </div>
     );
   }
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -24,6 +24,24 @@ export default {
         exclude: /node_modules/, 
         include: [path.resolve(__dirname, 'app')]
       },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'app')]
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader',
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'app')]
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader',
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'assets')]
+      }
     ],
   },
 
